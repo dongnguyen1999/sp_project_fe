c14260c66aa97d8e0ddd4df5547d319278d4aab0
Nguyễn Đông <ndong@Nguyens-iMac.local>
2021-06-07 10:55:46 +0700

    Integrate API cancel update

--- src/views/UploadForm/index.jsx
+++ src/views/UploadForm/index.jsx
@@ -9,6 +9,8 @@ import axios from 'axios';
 import NotificationModal from '../../components/NotificationModal';
 import { useEffect, useRef, useState } from 'react';
 import ProgressBarModal from '../../components/ProgressBarModal';
+import moment from 'moment';
+import { MD5 } from 'crypto-js';
 
 function UploadForm(props) {
   const { register, handleSubmit } = useForm();
@@ -18,17 +20,17 @@ function UploadForm(props) {
   ]
   let [fileTypeIndex, setFileTypeIndex] = useState(0);
   let [uploadProgress, setUploadProgress] = useState(0);
-  let [submitForm, setSubmitForm] = useState(true);
   let [fileSelected, setFileSelected] = useState(false);
   let [fileName, setFileName] = useState('');
-  let [showNotification, setShowNotification] = useState({
-    isLoading: false,
-    willShow: false,
+  let [isLoading, setIsLoading] = useState(false);
+  let [showNotification, setShowNotification] = useState(false);
+  let [hashKey, setHashKey] = useState(null);
+  let [notification, setNotification] = useState({
     error: false,
     title: 'Information Notification',
     message: 'Message',
   });
-  const submitFormRef = useRef(submitForm);
+  const isLoadingRef = useRef(false);
 
   const handleChangeFile = (event) => {
     let selected = event.target.files.length > 0;
@@ -37,20 +39,17 @@ function UploadForm(props) {
       setFileName(event.target.files[0].name);
     }
   }
-
-  
   
   const submitFormHandler = async (data) => {
-    console.log(data);
-    setShowNotification({willShow: false, isLoading: false});
-    setSubmitForm(true);
+    let timestamp = moment().format("MM ddd, YYYY HH:mm:ss a");
+    let hash = MD5(`${fileName} ${timestamp}`).toString();
+    setHashKey(hash);
     let squad_name = data?.squad_name;
     if (fileTypeIndex === 0 && squad_name) {
       let isValidSquadName = await checkClassName(squad_name);
       if (!isValidSquadName) {
-        setShowNotification({
-          willShow: true,
-          isLoading: false,
+        setIsLoading(false);
+        setNotification({
           error: true,
           title: 'Existed squad name',
           body: 'This squad name is exist! Please enter another name!',
@@ -62,11 +61,10 @@ function UploadForm(props) {
       file: data.xlsx[0],
       squad_name: data?.squad_name,
       file_type: fileTypes[fileTypeIndex].value,
+      hash: hash,
     }
     if (fileTypeIndex === 0 && !submitData.squad_name) {
-      setShowNotification({
-        willShow: true,
-        isLoading: false,
+      setNotification({
         error: true,
         title: 'Require squad name',
         body: 'Squad name is required! Please enter squad name!',
@@ -74,44 +72,28 @@ function UploadForm(props) {
       return;
     }
     if (!submitData.file) {
-      setShowNotification({
-        willShow: true,
-        isLoading: false,
+      setNotification({
         error: true,
         title: 'Require file',
         body: 'Please pick your xlsx file!',
       });
       return;
     }
-    // setData(submitData);
-    setShowNotification({willShow: false, isLoading: true});
+
+    // console.log('submitData', submitData);
+    setIsLoading(true);
+    postUploadXlsx(submitData);
+    
     await progressSequential([
       {time: 500, start:0, end:30},
       {time: 1000, start:30, end:70},
-      {time: 500, start:70, end:90},
-      {time: 200, start:90, end:95},
+      {time: 800, start:70, end:90},
+      {time: 200, start:90, end:100},
     ]);
 
-    // console.log(submitForm);
-    if (submitFormRef.current) {
-      const url = '/api/upload-xlsx';
-      const formData = new FormData();
-      for (const [key, value] of Object.entries(submitData)) {
-        console.log(`${key}: ${value}`);
-        if (key === 'xlsx') {
-          formData.append('xlsx',value[0]);
-        }
-        else formData.append(key ,value);
-      }
-      const config = {
-          headers: {
-              'content-type': 'multipart/form-data',
-              'Access-Control-Allow-Origin': '*'
-          },
-          crossdomain: true,
-      }
-      postUploadXlsx(url, formData, config);
-    }
+    if (isLoadingRef.current) setShowNotification(true);
+    setIsLoading(false);
+    cleanUpdateProcess(hash);
   }
 
   const executeUploadProgress = async ({time, start=0, end=90}) => {
@@ -137,18 +119,14 @@ function UploadForm(props) {
       if (response.status === 200) {
         return response.data;
       } else {
-        setShowNotification({
-          willShow: true,
-          isLoading: false,
+        setNotification({
           error: true,
           title: 'Error',
           body: 'Internal server error!',
         });
       }
     }).catch(error => {
-      setShowNotification({
-        willShow: true,
-        isLoading: false,
+      setNotification({
         error: true,
         title: 'Error',
         body: `Error occurred: ${error}`,
@@ -156,40 +134,90 @@ function UploadForm(props) {
     });
   }
 
-  const postUploadXlsx = (url, formData, config) => {
+  const postUploadXlsx = (submitData) => {
+    const url = '/api/upload-xlsx';
+    const formData = new FormData();
+    for (const [key, value] of Object.entries(submitData)) {
+      console.log(`${key}: ${value}`);
+      formData.append(key, value);
+    }
+    const config = {
+        headers: {
+            'content-type': 'multipart/form-data',
+            'Access-Control-Allow-Origin': '*'
+        },
+        crossdomain: true,
+    }
     axios.post(url, formData, config).then(response => response.data).then(response => {
-    console.log({response});
+      console.log({response});
       if (response && response.errorCode === 0) {
-        setShowNotification({
-          willShow: true,
-          isLoading: false,
+        setNotification({
           error: false,
           title: 'Successfully',
           body: response.message,
         });
       } else {
-        setShowNotification({
-          willShow: true,
-          isLoading: false,
+        setNotification({
           error: true,
           title: 'Error',
           body: response.message,
         });
       }
     }).catch(error => {
-      setShowNotification({
-        willShow: true,
-        isLoading: false,
+      setNotification({
         error: true,
-        title: 'Error',
+        title: 'Error from upload',
         body: `Error occurred: ${error}`,
       });
     });
   } 
 
   useEffect(() => {
-    submitFormRef.current = submitForm;
-  }, [submitForm])
+    if (notification.error == true && !isLoading) {
+      setShowNotification(true);
+    }
+  }, [notification]);
+
+  const cancelUploadXlsx = async () => {
+    console.log('send request cancel');
+    const url = '/api/upload-xlsx';
+    const submitData = {
+      type: 0,
+      hash: hashKey,
+    }
+    const config = {
+        headers: {
+            'content-type': 'multipart/form-data',
+            'Access-Control-Allow-Origin': '*'
+        },
+        crossdomain: true,
+        data: submitData
+    }
+    axios.delete(url, config);
+    setIsLoading(false);
+  }
+
+  const cleanUpdateProcess = (hash) => {
+    console.log('send request clean');
+    const url = '/api/upload-xlsx';
+    const submitData = {
+      type: 1,
+      hash: hash,
+    }
+    const config = {
+        headers: {
+            'content-type': 'multipart/form-data',
+            'Access-Control-Allow-Origin': '*'
+        },
+        crossdomain: true,
+        data: submitData
+    }
+    axios.delete(url, config);
+  }
+
+  useEffect(() => {
+    isLoadingRef.current = isLoading;
+  }, [isLoading])
 
   return (
     <Container className="pt-4 pb-4">
@@ -239,20 +267,20 @@ function UploadForm(props) {
         </Container>
 
         <ProgressBarModal  
-          show={showNotification.isLoading} 
+          show={isLoading} 
           title='Uploading content'
           message={`Uploading ${fileTypes[fileTypeIndex].value} file...`}
           variant={'primary'}
           progress={uploadProgress}
-          onCancel={() => {setShowNotification(showNotification => ({...showNotification, isLoading: false, willShow: false})); setSubmitForm(false)}}
+          onCancel={cancelUploadXlsx}
         />
 
         <NotificationModal 
-          show={showNotification.willShow} 
-          title={showNotification.title} 
-          body={showNotification.body}
-          variant={showNotification.error ? 'danger': 'success'}
-          onClose={() => setShowNotification(showNotification => ({...showNotification, isLoading: false, willShow: false}))}
+          show={showNotification} 
+          title={notification.title} 
+          body={notification.body}
+          variant={notification.error ? 'danger': 'success'}
+          onClose={() => setShowNotification(false)}
         />
         
       </Form>  
